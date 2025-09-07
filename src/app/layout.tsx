3f868067de4d0d7f8c17b9d34a050fee2a2fa5d5
Akshit Sivaraman <akshit.sivaraman@gmail.com>
2025-09-07 01:20:43 +0000

    change the favicon of the website to a traffic light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3e%3crect x='30' y='10' width='40' height='80' rx='10' fill='black'/%3e%3ccircle cx='50' cy='30' r='10' fill='red'/%3e%3ccircle cx='50' cy='50' r='10' fill='yellow'/%3e%3ccircle cx='50' cy='70' r='10' fill='green'/%3e%3c/svg%3e" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
